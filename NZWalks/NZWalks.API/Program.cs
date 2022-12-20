650156a759b817ce05c9ef01912673579437acc5
preetinv <pvalkeri.cs@gmail.com>
2022-12-20 18:16:15 +0530

    Swagger Authentication

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
 using NZWalks.API.Repository;
 using System.Text;
@@ -13,7 +14,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(Options=>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name="JWT Authentication",
+        Description="Enter a valid JWT bearer token",
+        In=ParameterLocation.Header,
+        Type=SecuritySchemeType.Http,
+        Scheme="bearer",
+        BearerFormat="Jwt",
+        Reference=new OpenApiReference
+        {
+            Id=JwtBearerDefaults.AuthenticationScheme,
+            Type=ReferenceType.SecurityScheme
+        }
+    };
+    Options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    Options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme,new string[] { } }
+    });
+});
+
 builder.Services.AddFluentValidation(Options => Options.RegisterValidatorsFromAssemblyContaining<Program>());
 
 builder.Services.AddDbContext<NZWalksDBContext>(Options => {
